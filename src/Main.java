5bfcbf127a3c2a6076db32cc8c2cd99caaf72b84
Hector NoteBook <hectorroberto7543@gmail.com>
2023-07-11 19:31:54 -0300

    Testando primeiros comandos no Java

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("The Miracle Club");
+        int ano = 2023;
+        System.out.println("O ano do filme: " + ano);
+        double duracaoMin = 91;
+        System.out.println("Duração: "+ duracaoMin + " Minutos");
+
+
+    }
+    }
