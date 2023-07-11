f7e406473cba12ee97cfc15ea65ac2306b3b842f
Hector NoteBook <hectorroberto7543@gmail.com>
2023-07-11 20:35:34 -0300

    Jogo simples de adivinhacao, testando while loop e scanner juntos!

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,29 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args)
     {
-        String nomeFilme = "The Miracle Club";
-        int ano = 2023;
-        double duracaoMin = 91;
-
-        System.out.println("Esse é o Screen Match");
-        System.out.println("Filme: "+nomeFilme);
-        System.out.println("O ano do filme: " + ano);
-        System.out.println("Duração: "+ duracaoMin + " Minutos");
-
-        // Utilizando o text BLocks, usa-se 3 pares de dupla aspas.
-        String sinopse =
-                """
-                There's just one dream for the women of Ballygar 
-                to taste freedom: to win a pilgrimage to the sacred French town of Lourdes.
-                """;
-        boolean disponivel = true;
-        if (disponivel)
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Bem Vindo ao jogo de adivinhação!");
+        System.out.println("Advinhe a fruta!");
+        boolean naoAcertou = true;
+        String nomeFruta = "Banana";
+        String palpite;
+        while (naoAcertou)
         {
-            System.out.println(String.format("O filme %s está disponível!", nomeFilme));
-        } else
-        {
-            System.out.println("Filme indisponível");
+            System.out.println("Digite o nome da fruta: ");
+            palpite = leitura.nextLine();
+            if (palpite.equals(nomeFruta))
+            {
+                System.out.println("Parabens você acertou!");
+                naoAcertou = false;
+            }else
+            {
+                System.out.println("Errou! Tente novamente!");
+            }
+
         }
 
     }
